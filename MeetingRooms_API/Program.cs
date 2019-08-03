21b51545fed1262102a7f29328263241a2e6dcf2
Marcos <kakoferrare87@gmail.com>
2019-08-03 15:44:28 -0300

    Porta default

--- MeetingRooms_API/Program.cs
+++ MeetingRooms_API/Program.cs
@@ -14,7 +14,7 @@ namespace MeetingRooms_API
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).UseUrls("http://localhost:5001/").Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
